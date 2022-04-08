bc82f119d165cf99fb24898c20692ffde4ec4d1d
Dariia <grimalskayad@gmail.com>
2022-04-08 11:20:30 +0300

    feat: BR-1272

--- wrappers/user_go/utils.go
+++ wrappers/user_go/utils.go
@@ -1,20 +1,10 @@
 package user_go
 
-import "fmt"
-
 func getFirstAndLastNameWithPrivacy(status NamePrivacyStatus, firstName string, lastName string, userName string) (string, string) {
 	if status == NamePrivacyStatusAllHidden || status == NamePrivacyStatusFirstNameHidden ||
 		status == NamePrivacyStatusLastNameHidden || (len(firstName) == 0 && len(lastName) == 0) {
-		return formatUserName(userName), ""
+		return userName, ""
 	}
 
 	return firstName, lastName
-}
-
-func formatUserName(userName string) string {
-	if len(userName) == 0 {
-		return ""
-	}
-
-	return fmt.Sprintf("@%v", userName)
 }
